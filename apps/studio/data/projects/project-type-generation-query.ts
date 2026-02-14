01f9f70cbb7b8ff75709cd30e6fb23dd7d31e672
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-02-14 10:09:57 +0100

    Fixed typescript type generation via postgrest

--- apps/studio/data/projects/project-type-generation-query.ts
+++ apps/studio/data/projects/project-type-generation-query.ts
@@ -4,19 +4,36 @@ import { get, handleError } from 'data/fetchers'
 import type { ResponseError } from 'types'
 import { projectKeys } from './keys'
 
-export type GenerateTypesVariables = { slug?: string, ref?: string; included_schemas?: string }
+export type GenerateTypesVariables = {
+  slug?: string
+  ref?: string
+  branch?: string
+  included_schemas?: string
+}
 
 export async function generateTypes(
-  { slug, ref, included_schemas }: GenerateTypesVariables,
+  { slug, ref, branch, included_schemas }: GenerateTypesVariables,
   signal?: AbortSignal
 ) {
   if (!slug) throw new Error('Organization slug is required')
   if (!ref) throw new Error('Project ref is required')
 
-  const { data, error } = await get(`/platform/organizations/{slug}/projects/{ref}/types/typescript`, {
-    params: { path: { slug, ref }, query: { included_schemas } },
-    signal,
-  })
+  const { data, error } = await get(
+    `/platform/organizations/{slug}/projects/{ref}/types/typescript`,
+    {
+      params: {
+        path: {
+          slug,
+          ref,
+        },
+        query: {
+          included_schemas,
+          branch,
+        },
+      },
+      signal,
+    }
+  )
 
   if (error) handleError(error)
   return data
@@ -26,11 +43,11 @@ export type GenerateTypesData = Awaited<ReturnType<typeof generateTypes>>
 export type GenerateTypesError = ResponseError
 
 export const useGenerateTypesQuery = <TData = GenerateTypesData>(
-  { slug, ref, included_schemas }: GenerateTypesVariables,
+  { slug, ref, branch, included_schemas }: GenerateTypesVariables,
   { enabled = true, ...options }: UseQueryOptions<GenerateTypesData, GenerateTypesError, TData> = {}
 ) =>
   useQuery<GenerateTypesData, GenerateTypesError, TData>(
-    projectKeys.types(slug, ref),
-    ({ signal }) => generateTypes({ slug, ref, included_schemas }, signal),
-    { enabled: enabled && typeof ref !== 'undefined' && typeof slug !== 'undefined', ...options }
+    projectKeys.types(slug, ref, branch),
+    ({ signal }) => generateTypes({ slug, ref, branch, included_schemas }, signal),
+    { enabled: enabled && typeof ref !== 'undefined' && typeof slug !== 'undefined' && typeof branch !== 'undefined', ...options }
   )

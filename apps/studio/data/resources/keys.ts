6a6ec79045ab8273dc92e97dcaacd24bbbd5d948
ebrahim savari <106956085+EbiRider@users.noreply.github.com>
2026-02-24 08:06:31 +0330

    fixing a cache issue which prevented metering request from triggering

--- apps/studio/data/resources/keys.ts
+++ apps/studio/data/resources/keys.ts
@@ -1,6 +1,8 @@
 export const resourcesKeys = {
-  organizationUsage: (orgSlug?: string) =>
-    ['organizations', orgSlug, 'resources', 'usage'] as const,
-  projectUsage: (orgSlug?: string, projectRef?: string) =>
-    ['projects', orgSlug, projectRef, 'resources', 'usage'] as const,
+  organizationUsage: (orgSlug?: string, start?: string, end?: string) =>
+    ['organizations', orgSlug, 'resources', 'usage', start, end] as const,
+  organizationMetering: (orgSlug?: string, start?: string, end?: string) =>
+    ['organizations', orgSlug, 'resources', 'metering', start, end] as const,
+  projectUsage: (orgSlug?: string, projectRef?: string, start?: string, end?: string) =>
+    ['projects', orgSlug, projectRef, 'resources', 'usage', start, end] as const,
 }
